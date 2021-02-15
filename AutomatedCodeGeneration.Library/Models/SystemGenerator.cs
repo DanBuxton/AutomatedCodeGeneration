c7baf46eab8a7b07cf42b9a8eb22404186a60881
DanBuxton <dan.buxton99@gmail.com>
2021-02-15 22:06:40 +0000

    Added boilerplate async static method to generater system.
    Added file structure and a public class to start the generation process.

---
+++ AutomatedCodeGeneration.Library/Models/SystemGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedCodeGeneration.Library.Models
+{
+    public sealed class SystemGenerator
+    {
+        public static async Task<Result> CreateSystem(dynamic systemInfo)
+        {
+            await Task.Run(() =>
+            {
+                //TODO: Generate the system
+            });
+
+            throw new NotImplementedException();
+        }
+
+        public sealed record Result
+        {
+            public bool HasError { get => Errors != null; }
+
+            public string[] Errors { get; private init; } = null;
+
+            public Result(string[] errors)
+            {
+                Errors = errors;
+            }
+
+            public void Deconstruct(out string[] errorMsgs) => (errorMsgs) = (Errors);
+        }
+    }
+}
