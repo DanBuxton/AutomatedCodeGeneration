4ef4cd4efd1235b36ddd6c911fb018b14b6d36a6
DanBuxton <dan.buxton99@gmail.com>
2021-02-21 22:54:27 +0000

    Added quick test data

---
+++ AutomatedCodeGeneration.Tests/HelperTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using AutomatedCodeGeneration.Models;
+using Xunit;
+
+namespace AutomatedCodeGeneration.Tests
+{
+    public class HelperTests
+    {
+        [Theory]
+        [InlineData("CSharp")]
+        [InlineData("CSharP")]
+        [InlineData("CSHArP")]
+        [InlineData("csharp")]
+        [InlineData("CSHARP")]
+        public void Language_Exists_Test(string lang)
+        {
+            var result = Helper.LanguageExists(lang);
+
+            Assert.NotNull(result);
+        }
+
+        [Theory]
+        [InlineData("ThisLanguageDoesNotExist")]
+        [InlineData(null)]
+        public void Language_Not_Exist_Test(string lang)
+        {
+            var result = Helper.LanguageExists(lang);
+
+            Assert.Null(result);
+        }
+
+        [Theory]
+        [InlineData("aef3af89-f90a-4670-bcb0-ff7693325faa", "CSharp", ".")]
+        [InlineData("aef3af89-f90a-4670-bcb0-ff7693325faa", "CSharp")]
+        public void CreateSystemInfo_Test(string id, string lang, string output = null)
+        {
+            var goodId = Guid.Parse(id);
+
+            var (actualId, actualLang, actualOutput) = Helper.CreateSystemInfo(goodId, lang, output);
+
+            Assert.Equal(goodId, actualId);
+            Assert.Equal(lang, actualLang);
+            Assert.Equal(output ?? Directory.GetCurrentDirectory(), actualOutput);
+        }
+    }
+}
---
+++ AutomatedCodeGeneration.Tests/UseCaseTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AutomatedCodeGeneration.Library;
+using AutomatedCodeGeneration.Models;
+using Xunit;
+
+namespace AutomatedCodeGeneration.Tests
+{
+    public class UseCaseTests
+    {
+        [Theory]
+        [InlineData("aff3af89-f90a-4670-bcb0-ff7693325faa", "CSharp", ".")]
+        [InlineData("aff3af89-f90a-4670-bcb0-ff7693325fab", "CSharp")]
+        [InlineData("aff3af89-f90a-4670-bcb0-ff7693325fac", "Python")]
+        [InlineData("aff3af89-f90a-4670-bcb0-ff7693325fad", "JavaScript")]
+        public async void Main_Test(string id, string lang, string output = null)
+        {
+            var goodId = Guid.Parse(id);
+
+            var result = await SystemGenerator.CreateSystem(new SystemInfo(goodId, lang, output));
+
+            Assert.True(result.HasError);
+        }
+    }
+}
