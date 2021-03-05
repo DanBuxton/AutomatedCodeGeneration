ee4cb954ca6b7400d69212e7c1236895a9571f87
Daniel Buxton <dan.buxton99@gmail.com>
2021-03-05 22:00:16 +0000

    Added quick test data for File model and builder

---
+++ AutomatedCodeGeneration.DataLayer.Tests/FileBuilderTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using AutomatedCodeGeneration.DataLayer.Files.Builders;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AutomatedCodeGeneration.DataLayer.Tests
+{
+    public sealed class FileBuilderTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public FileBuilderTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        [Fact]
+        public void CSharp_Test()
+        {
+            const string indent = "    ", newLine = "\n\r";
+            var result = new CSharpFileBuilder(indent, newLine)
+                .WithImports(new List<string> {"System"})
+                .WithNamespace("ACG").WithClassName("Test")
+                .WithClassAccess(AccessType.Public)
+                .Build();
+
+            var expected = $"using System;{newLine}{newLine}namespace ACG{newLine}{{{newLine}{indent}public class Test{newLine}" +
+                           $"{indent}{{{newLine}{indent}{indent}{newLine}{indent}}}{newLine}}}";
+
+#if DEBUG
+            _output.WriteLine($"Expected:\n\n{expected}\n");
+            _output.WriteLine($"Result:\n\n{result}");
+            _output.WriteLine($"Result:\n\n{Environment.NewLine}");
+#endif
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
