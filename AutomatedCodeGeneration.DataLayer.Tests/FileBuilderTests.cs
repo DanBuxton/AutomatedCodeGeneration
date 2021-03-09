020e54592f90f6517b832f61712ca7d95625e0ff
Daniel Buxton <dan.buxton99@gmail.com>
2021-03-09 14:48:35 +0000

    Added more file builder tests.

--- AutomatedCodeGeneration.DataLayer.Tests/FileBuilderTests.cs
+++ AutomatedCodeGeneration.DataLayer.Tests/FileBuilderTests.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using AutomatedCodeGeneration.DataLayer.Files.Builders;
 using Xunit;
 using Xunit.Abstractions;
@@ -16,18 +14,52 @@ namespace AutomatedCodeGeneration.DataLayer.Tests
             _output = output;
         }
 
-        [Fact]
-        public void CSharp_Test()
+        [Theory]
+        [InlineData("\n", "\t", "AutoMatedCodeGeneration.CLI", "Program", AccessType.Public, "System", "System.Collections.Generic")]
+        [InlineData("\n\r", "\t", "", "TestClassName", AccessType.Public, "System", "System.Collections.Generic", "System.Linq")]
+        [InlineData("\n\r", "\t", "AutoMatedCodeGeneration.CLI", "TestClassName", AccessType.Public, "System")]
+        [InlineData("\n\r", "\t", "AutoMatedCodeGeneration.CLI", "TestClassName", AccessType.Public, "System", "System.Collections.Generic")]
+        [InlineData("\n\r", "", "AutoMatedCodeGeneration.CLI", "TestClassName", AccessType.Public, "System", "System.Collections.Generic")]
+        [InlineData("\n", "\t", "AutoMatedCodeGeneration.CLI", "TestClassName", AccessType.Public, "System", "System.Collections.Generic")]
+        [InlineData("\n", "", "AutoMatedCodeGeneration.CLI", "TestClassName", AccessType.Public, "System", "System.Collections.Generic")]
+        [InlineData("", "", "AutoMatedCodeGeneration.CLI", "TestClassName", AccessType.Public, "System", "System.Collections.Generic")]
+        [InlineData("\n", "    ", "AutoMatedCodeGeneration.CLI", "TestClassName", AccessType.Public)]
+        public void CSharp_Test(string newLine, string indent, string ns, string name, AccessType access, params string[] imports)
         {
-            const string indent = "    ", newLine = "\n\r";
+            //const string indent = "    ", newLine = "\n\r";
             var result = new CSharpFileBuilder(indent, newLine)
-                .WithImports(new List<string> {"System"})
-                .WithNamespace("ACG").WithClassName("Test")
-                .WithClassAccess(AccessType.Public)
+                .WithImports(imports)
+                .WithNamespace(ns).WithClassName(name)
+                .WithClassAccess(access)
                 .Build().ToString();
 
-            var expected = $"using System;{newLine}{newLine}namespace ACG{newLine}{{{newLine}{indent}public class Test{newLine}" +
-                           $"{indent}{{{newLine}{indent}{indent}{newLine}{indent}}}{newLine}}}";
+            var expected = "";
+
+            if (imports.Length > 0)
+            {
+                expected += $"using {string.Join($";{newLine}using ", imports)};{newLine}{newLine}";
+            }
+
+            var i = 0;
+
+            if (!string.IsNullOrEmpty(ns))
+            {
+                expected += $"namespace {ns}{newLine}{{{newLine}";
+                expected = Indent(expected, indent, ++i);
+            }
+
+            expected += $"public class {name}{newLine}";
+            expected = Indent(expected, indent, i);
+            expected += $"{{{newLine}";
+            expected = Indent(expected, indent, ++i);
+            expected += $"{newLine}";
+            expected = Indent(expected, indent, --i);
+            expected += "}";
+            if (!string.IsNullOrEmpty(ns))
+            {
+                expected += $"{newLine}}}";
+            }
+
 
 #if DEBUG
             _output.WriteLine($"Expected:\n\n{expected}\n");
@@ -36,6 +68,16 @@ namespace AutomatedCodeGeneration.DataLayer.Tests
 #endif
 
             Assert.Equal(expected, result);
+        }
+
+        private static string Indent(string expected, string indent, int i)
+        {
+            for (var j = 0; j < i; j++)
+            {
+                expected += indent;
+            }
+
+            return expected;
         }
     }
 }
