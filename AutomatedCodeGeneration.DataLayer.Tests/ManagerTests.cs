08761b9ff3672a773c836a06e93df1277a63aaaa
Daniel Buxton <dan.buxton99@gmail.com>
2021-03-29 16:24:08 +0100

    Added more Language Manager tests

--- AutomatedCodeGeneration.DataLayer.Tests/ManagerTests.cs
+++ AutomatedCodeGeneration.DataLayer.Tests/ManagerTests.cs
@@ -1,51 +1,73 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using AutomatedCodeGeneration.DataLayer.Diagrams;
 using AutomatedCodeGeneration.DataLayer.Diagrams.ClassDiagram;
-using AutomatedCodeGeneration.DataLayer.Files;
-using AutomatedCodeGeneration.DataLayer.Files.Builders;
 using AutomatedCodeGeneration.DataLayer.Managers;
 using Xunit;
 using Xunit.Abstractions;
+using static Xunit.Assert;
 
 namespace AutomatedCodeGeneration.DataLayer.Tests
 {
     public sealed class ManagerTests
     {
-        private readonly ITestOutputHelper _output;
-
-        public ManagerTests(ITestOutputHelper output)
+        [Theory]
+        [InlineData("csharp")]
+        //[InlineData("java")]
+        //[InlineData("python")]
+        public void Manager_Exists_Test(string language)
         {
-            _output = output;
+            var manager = Helper.GetLanguageManager(language, GetSystem());
+
+            NotNull(manager);
+        }
+
+        private static SystemModel GetSystem()
+        {
+            SystemModel system = new() { Namespace = "ACG" };
+            List<ClassModel> classes = new()
+            {
+                new ClassModel
+                {
+                    Methods = new List<ClassMethodModel>
+                    {
+                        new()
+                        {
+                            NameType = new NameTypeModel {Name = "Main", Type = "void", IsStatic = true},
+                            Params = new List<NameTypeModel>
+                            {
+                                new() {Name = "args", Type = "string[]"}
+                            },
+                            Access = AccessType.Public
+                        },
+
+                        new() {NameType = new NameTypeModel {Name = "SayHello", Type = "string", IsStatic = true}}
+                    },
+                    Namespace = "ACG.CLI",
+                    Access = AccessType.Internal,
+                    Name = "ExampleClass",
+                    System = system
+                }
+            };
+            system.Classes = classes;
+
+            return system;
+        }
+
+        [Theory]
+        [InlineData("brainfuck")]
+        public void Manager_Not_Exists_Test(string language)
+        {
+            Null(Helper.GetLanguageManager(language, GetSystem()));
         }
 
         [Fact]
         public void CSharp_Test()
         {
-            SystemModel system = new() { Namespace = "ACG" };
-            List<ClassModel> classes = new()
-            {
-                new ClassModel()
-                {
-                    Methods = new List<ClassMethodModel>()
-                    {
-                        new() {NameType = new NameTypeModel() { Name = "SayHello", Type = "string"}}
-                    },
-                    Data = new List<ClassDataModel>()
-                    {
+            var manager = new CSharpManager(GetSystem());
 
-                    },
-                    Namespace = "",
-                    Access = AccessType.Internal,
-                    Name = "ExampleClass",
-                    System = system,
-                }
-            };
-            system.Classes = classes;
-
-            var manager = Helper.GetLanguageManager("CshArp", system);
-
-            Assert.NotNull(manager);
-            Assert.True(manager is CSharpManager);
+            Equal(typeof(CSharpManager), manager.GetType());
         }
     }
 }
