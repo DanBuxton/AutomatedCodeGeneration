33d2f3113cc416e667d178360114015d5b81046c
Daniel Buxton <dan.buxton99@gmail.com>
2021-04-25 22:28:22 +0100

    minor update

--- AutomatedCodeGeneration.DataLayer/Files/Builders/CSharp/CSharpInterfaceFileBuilder.cs
+++ AutomatedCodeGeneration.DataLayer/Files/Builders/CSharp/CSharpInterfaceFileBuilder.cs
@@ -7,7 +7,7 @@ using AutomatedCodeGeneration.DataLayer.Files.Languages.CSharp;
 
 namespace AutomatedCodeGeneration.DataLayer.Files.Builders.CSharp
 {
-    public class CSharpInterfaceFileBuilder : IFileBuilder
+    public class CSharpInterfaceFileBuilder : IInterfaceFileBuilder
     {
         private readonly CSharpInterfaceFileModel _model;
 
@@ -16,56 +16,42 @@ namespace AutomatedCodeGeneration.DataLayer.Files.Builders.CSharp
             _model = new CSharpInterfaceFileModel(indent, newLine);
         }
 
-        public CSharpInterfaceFileBuilder WithImports([NotNull] List<string> imports)
+        public IInterfaceFileBuilder WithImports([NotNull] List<string> imports)
         {
             _model.Imports = imports.ToList();
 
             return this;
         }
 
-        public CSharpInterfaceFileBuilder WithNamespace(string ns)
+        public IInterfaceFileBuilder WithNamespace(string ns)
         {
             _model.Namespace = ns;
 
             return this;
         }
 
-        //public CSharpInterfaceFileBuilder WithAttributes(List<string> attributes)
-        //{
-        //    _model.Attributes = attributes;
+        public IInterfaceFileBuilder WithAttributes(List<string> attributes)
+        {
+            _model.Attributes = attributes;
 
-        //    return this;
-        //}
+            return this;
+        }
 
-        public CSharpInterfaceFileBuilder WithAccess(Enums.AccessType access)
+        public IInterfaceFileBuilder WithAccess(Enums.AccessType access)
         {
             _model.Access = access.AsLowerString();
 
             return this;
         }
 
-        public CSharpInterfaceFileBuilder WithName(string name)
+        public IInterfaceFileBuilder WithName(string name)
         {
             _model.FileName = name;
 
             return this;
         }
 
-        //public CSharpInterfaceFileBuilder WithFieldsAndProperties(List<ClassDataModel> fieldsAndProperties)
-        //{
-        //    _model.FieldsAndProperties = fieldsAndProperties;
-
-        //    return this;
-        //}
-
-        //public CSharpInterfaceFileBuilder WithConstructors(List<ClassMethodModel> constructors)
-        //{
-        //    _model.Constructors = constructors;
-
-        //    return this;
-        //}
-
-        public CSharpInterfaceFileBuilder WithMethods(List<ClassMethodModel> methods)
+        public IInterfaceFileBuilder WithMethods(List<ClassMethodModel> methods)
         {
             _model.Methods = methods;
 
